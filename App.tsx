04345bb216e5890fb090c5c43213b8ddcaaedeb0
nayeong lee <lny021102@gmail.com>
2024-08-29 03:19:51 +0900

    [INIT] start React Native Project

--- App.tsx
+++ App.tsx
@@ -76,20 +76,10 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="하이">
+           리액트 네이티브 메인화면!
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+
         </View>
       </ScrollView>
     </SafeAreaView>
